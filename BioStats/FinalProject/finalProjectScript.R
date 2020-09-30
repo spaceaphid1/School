21b8b0b1b6519ced581ff4ee0373467edf78dc7c
spaceaphid1 <spaceaphid@gmail.com>
2020-09-30 00:18:35 +0000

    starting work on final project

---
+++ BioStats/FinalProject/finalProjectScript.R
@@ -0,0 +1,29 @@
+#'Jackson's BioStats Final Project
+#'Packages
+
+library(tidyverse)
+library(readxl)
+library(nlme)
+library(lme4)
+
+#'Data Inspection
+
+richnessDat <- read_excel("~/Repos/School/BioStats/FinalProject/finalProjData.xlsx", 
+                            sheet = "Species Richness")
+head(richnessDat)
+
+#'Fert Treatment on total plant Species in given plot
+ggplot(richnessDat, aes(`Fertilizer Treatment`, `Total Spp #`)) +
+  geom_boxplot()
+
+#'Fert Treatment on total non-clonal plant Species in given plot
+
+ggplot(richnessDat, aes(`Fertilizer Treatment`, `NC Spp #`)) +
+  geom_boxplot()
+
+#'Fert Treatment on total clonal plant Species in given plot
+
+ggplot(richnessDat, aes(`Fertilizer Treatment`, `Clon Spp #`)) +
+  geom_boxplot()
+
+
